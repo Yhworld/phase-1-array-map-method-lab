97ad2e29ac55ae3c1c56140e8a6e18ac22c9eb2b
Yahya <yahyahussein670@gmail.com>
2022-06-11 13:27:29 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+const newTutorials = tutorials.map(el => {
+  const words = el.split(' ');
+  const wordsCapitalized = words.map(word => word[0].toUpperCase() + word.slice(1, word.length));
+  const finalTutorials = wordsCapitalized.join(' ');
+  return finalTutorials;
+});
+return newTutorials;
+}; 
